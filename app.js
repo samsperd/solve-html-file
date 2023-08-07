dee11940048e56badc0ccbddbf58f240b31c5e7b
samsperd <iloejeobinna@gmail.com>
2023-08-07 23:14:14 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const posthtml = require('posthtml');
+const inlineAssets = require('posthtml-inline-assets');
+
+const app = express();
+const port = 3000;
+
+// Middleware to parse incoming JSON data
+app.use(bodyParser.json());
+
+// API endpoint to process HTML content
+app.post('/process-html', (req, res) => {
+  // Get the HTML content from the request body
+  const htmlContent = req.body.html;
+
+  // Process the HTML content with PostHTML-Inline-Assets package
+  posthtml()
+    .use(inlineAssets())
+    .process(htmlContent)
+    .then((result) => {
+      // Send back the output HTML as the API response
+      res.send(result.html);
+    })
+    .catch((error) => {
+      // In case of any errors, send an error response
+      res.status(500).send('Error processing HTML content');
+    });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
